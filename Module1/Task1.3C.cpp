e3388b48b69d86a06ef67eac6003c54a190da4e9
advanced-mn <141113805+advanced-mn@users.noreply.github.com>
2023-08-04 12:40:45 +1000

    Update Task1.3C.cpp
    Changes the way it work

--- Module1/Task1.3C.cpp
+++ Module1/Task1.3C.cpp
@@ -1,8 +1,7 @@
-static int echo_pin = 5;
-static int tri_pin = 4;
-
 static int led_pin = 13;
 static int sensor_pin = 2;
+static int sensor2_pin = 3;
+
 
 void setup()
 {
@@ -10,47 +9,31 @@ void setup()
   pinMode(led_pin, OUTPUT);
   // Set up the pin to input mode
   pinMode(sensor_pin, INPUT);
-  
-  // led and temp set up
-  pinMode(tri_pin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echo_pin, INPUT); // Sets the echoPin as an Input
+
+  pinMode(sensor2_pin, INPUT);
   
   attachInterrupt(digitalPinToInterrupt(sensor_pin),
+                  ISR_sensor, CHANGE);
+  
+  attachInterrupt(digitalPinToInterrupt(sensor2_pin),
                   ISR_sensor, CHANGE);
   
   Serial.begin(9600);
 }
 
-void loop(){}
-
-void ultrasonic(){
-  float distance;
-  float duration;
-  
-  digitalWrite(tri_pin, LOW);
-  digitalWrite(tri_pin, HIGH);
-  digitalWrite(tri_pin, LOW);
-
-  
-  // Measure the pulse duration on the ECHO pin
-  duration = pulseIn(echo_pin, HIGH);
-  
-  // Calculate the distance in centimeters
-  distance = duration * 0.034 / 2;
-  
-  Serial.print("Distance: ");
-  Serial.println(distance);
-}
-
 void ISR_sensor()
 {  
-  if (digitalRead(led_pin) == LOW){
+  if (digitalRead(sensor_pin) == HIGH || 
+      digitalRead(sensor2_pin) == HIGH){
     digitalWrite(led_pin, HIGH);
     Serial.println("ON");
-    ultrasonic();
+
   } else
   {
   	digitalWrite(led_pin, LOW);
     Serial.println("OFF");
   }
+}
+
+void loop(){
 }
